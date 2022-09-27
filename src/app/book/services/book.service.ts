903f2d5d80164802f9b2bf33ff0dc204a55af88a
nathandgp <npolitico@77global.biz>
2022-09-27 16:51:55 +0800

    update4

--- src/app/book/services/book.service.ts
+++ src/app/book/services/book.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { FormArray } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
+
+  booksArray: FormArray
 
   constructor() { }
 }
