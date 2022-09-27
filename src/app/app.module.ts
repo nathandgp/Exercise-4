d2ca84983f5c8cbcef31e7701413d6c72405ba60
nathandgp <npolitico@77global.biz>
2022-09-27 16:52:10 +0800

    update4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home/home.component';
 import { ProfileComponent } from './user/pages/profile/profile.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { CommandBarComponent } from './shared/components/command-bar/command-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ProfileComponent
+    ProfileComponent,
+    HeaderComponent,
+    CommandBarComponent
   ],
   imports: [
     BrowserModule,
