1d2580417cdca47739f0bd417ae4dea2bc8c56e8
nathandgp <npolitico@77global.biz>
2022-09-27 15:47:03 +0800

    update3

--- src/app/user/pages/profile/profile.component.ts
+++ src/app/user/pages/profile/profile.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { User } from '../../models/user';
 
 @Component({
@@ -6,24 +7,45 @@ import { User } from '../../models/user';
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent implements OnInit {
+export class ProfileComponent implements OnInit, AfterViewInit {
 
   profileName = "This is my profile page."
 
-  model: User = {
-    name: "",
-    email: "",
-    bio: "",
-    active: true
+  // model: User = {
+  //   name: '',
+  //   email: '',
+  //   bio: '',
+  //   active: true
+  // }
+
+  userForm: FormGroup
+
+  constructor(fb:FormBuilder){
+    this.userForm = fb.group({
+      name: [''],
+      email: [''],
+      bio: [''],
+      active: []
+    })
+  }
+  ngAfterViewInit(): void {
+    // this.userForm = fb.group({
+    //   name: [''],
+    //   email: [''],
+    //   bio: [''],
+    //   active: []
+    // })
   }
 
-  constructor() { }
 
   ngOnInit(): void {
+    this.userForm.valueChanges.subscribe((data) => {
+      console.log(data)
+    })
   }
 
 
   submit() {
-    console.log(this.model)
+    console.log(this.userForm.value)
   }
 }
